99a4322d774898b42c0d6b7f68aea7cae8d4085c
Binup karki <karki011@gmail.com>
2020-05-20 14:50:00 -0500

    inital code

--- bugs_bugs/settings.py
+++ bugs_bugs/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bugs.apps.BugsConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
