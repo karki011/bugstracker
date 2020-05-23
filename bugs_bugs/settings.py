507a05d05d45cc6685101d9a20ddf07b1b767f84
Binup karki <karki011@gmail.com>
2020-05-22 21:24:36 -0500

    changed database

--- bugs_bugs/settings.py
+++ bugs_bugs/settings.py
@@ -72,13 +72,24 @@ WSGI_APPLICATION = 'bugs_bugs.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'bugtrackers',
+            'USER': 'subash',
+            'PASSWORD': 'Sub@sh1988@@',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -116,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # custom user
 AUTH_USER_MODEL = "account.Account"
