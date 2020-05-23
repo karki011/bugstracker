a602e539a02f7f6e8c43c84c033f5338f51908c0
Binup karki <karki011@gmail.com>
2020-05-22 21:47:31 -0500

    changed static root

--- bugs_bugs/settings.py
+++ bugs_bugs/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-]
+)
 # custom user
 AUTH_USER_MODEL = "account.Account"
 
